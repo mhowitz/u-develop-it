6ba0d1806c569afc717c1b9c3cdfcca6a0a0caf2
mikwitz <mikaylahowitz@gmail.com>
2022-01-13 17:48:13 -0700

    connected db to server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { APPCENTER } = require('ci-info');
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +6,24 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //YourMySql username,
+        user: 'root',
+        //YOUR mysql password
+        password: 'Bc1033273.',
+        database: 'election'
+    },
+    console.log('Connected to the election database!')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //DEFAULT RESPONSE FOR ANY OTHER REQUEST(NOT FOUND)
 app.use((req, res) => {
